cee89a364cf38f13c517dc6d8d315c446b8e9b55
Karisson Leonardo da Silva <107573950+kaon15@users.noreply.github.com>
2024-11-06 14:48:54 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(1400, 1400, WEBGL); //plano 3D
+  createCanvas(1920, 1080, WEBGL); //plano 3D
     terra=loadImage('terra.jpg')
 }
 
@@ -7,5 +7,5 @@ function draw() {
   background(0, 0, 0); //cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(135, 160); //esfera 3D
+  sphere(535, 560); //esfera 3D
 }
