7fca54a50308c8e2135c0ba028e72c2266478d75
Karisson Leonardo da Silva <107573950+kaon15@users.noreply.github.com>
2024-11-06 14:49:04 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
 
 function draw() {
   background(0, 0, 0); //cor de fundo
-  rotateY(millis()/1000); // rotação da terra
+  rotateY(millis()/100); // rotação da terra
   texture(terra); //textura da terra, imagem.
   sphere(535, 560); //esfera 3D
 }
