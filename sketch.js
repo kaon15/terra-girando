2959a2726bd96b8d6ec305e9d730f183b31a6a56
Karisson Leonardo da Silva <107573950+kaon15@users.noreply.github.com>
2024-11-06 14:38:40 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400, WEBGL); //plano 3D
+    terra=loadImage('terra.jpg')
+}
+
+function draw() {
+  background(0, 0, 0); //cor de fundo
+  rotateY(millis()/1000); // rotação da terra
+  texture(terra); //textura da terra, imagem.
+  sphere(135, 160); //esfera 3D
+}
